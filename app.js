0178974d92dae89f6dfcf608035978a7a1879772
Anthony Ranieri <anthonyjaranieri@gmail.com>
2023-06-15 10:57:45 -0400

    server.use code in app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const usersRoutes = require("./routes/users");
+const itemsRoutes = require("./routes/clothingItems");
 
 const { PORT = 3001 } = process.env;
 const server = express();
+
+server.use("/users", usersRoutes);
+server.use("/clothingItems", itemsRoutes);
 
 server.listen(PORT, () => {
   console.log("Everything works fine");
